0386954008052cd9f62b83faf1a49542503cea1d
shubham2o <2contactshubham@gmail.com>
2024-08-06 01:08:26 +0530

    Work component has been completed, but no animation has been added yet (48:44)

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,12 +1,65 @@
 const Work = () => {
+    const images = [
+        {
+            url: "https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png",
+            top: "0%",
+            left: "40%",
+            isActive: false,
+        },
+        {
+            url: "https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png",
+            top: "4%",
+            left: "37%",
+            isActive: false,
+        },
+        {
+            url: "https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png",
+            top: "-6.5%",
+            left: "42.8%",
+            isActive: false,
+        },
+        {
+            url: "https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/634ef092455ce2cf591e52d1_Rainfall.png",
+            top: "6.4%",
+            left: "40%",
+            isActive: false,
+        },
+        {
+            url: "https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/634ef0ac7e7179d210dc41f0_Summon.png",
+            top: "0%",
+            left: "44.8%",
+            isActive: false,
+        },
+        {
+            url: "https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/634ef0af108a465002975acd_Showcase%20Websites%20(1).png",
+            top: "-3%",
+            left: "38.5%",
+            isActive: false,
+        },
+    ];
+
     return (
-        <div className="mt-4 w-full h-[82vh] text-center">
+        <div className="w-full h-[82vh] text-center">
             <div className="max-w-screen-xl h-full mx-auto flex flex-col items-center">
-                <div className="h-full font-medium flex justify-center items-center overflow-hidden">
-                    <h1 className="w-full text-[36.3em]">work</h1>
+                <div className="w-full font-medium flex justify-center items-center overflow-hidden">
+                    <h1 className="w-full text-[36.3em] relative">work</h1>
+
+                    <div className="w-full h-full absolute top-[39.5%]">
+                        {images.map((elem, index) => (
+                            elem.isActive
+                            &&
+                            <img
+                                key={index}
+                                src={elem.url}
+                                alt="card-images"
+                                className="w-72 mt-3 rounded-2xl absolute"
+                                style={{ top: elem.top, left: elem.left }}
+                            />
+                        ))}
+                    </div>
                 </div>
 
-                <p className="w-full pb-4 text-[1.15em] text-[#919191]">Web Design, Webflow Development, Creative Development</p>
+                <p className="w-full text-[1.16em] text-[#919191]">Web Design, Webflow Development, Creative Development</p>
             </div>
         </div>
     )
