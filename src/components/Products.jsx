888a1dc7709ada0f1a57079e8609e2b86a6da5fd
shubham2o <2contactshubham@gmail.com>
2024-08-07 00:58:16 +0530

    Working on Products component styling and functionality

---
+++ src/components/Products.jsx
@@ -0,0 +1,15 @@
+import Product from "./Product";
+
+const Products = () => {
+    const products = [
+        {},
+    ];
+
+    return (
+        <div className="w-full h-full my-36">
+            <Product />
+        </div>
+    )
+}
+
+export default Products;
