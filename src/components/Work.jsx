cb64057a6f776962e2130256d9428e326ddc7a5e
shubham2o <2contactshubham@gmail.com>
2024-08-09 19:31:22 +0530

    Scroll animation on Work component - added (31:35)

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
+import { useScroll } from "framer-motion";
+
 const Work = () => {
-    const images = [
+    const [images, setImages] = useState([
         {
             url: "https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png",
             top: "0%",
@@ -36,7 +39,45 @@ const Work = () => {
             left: "38.5%",
             isActive: false,
         },
-    ];
+    ]);
+
+    const { scrollYProgress } = useScroll();
+
+    scrollYProgress.on("change", (data) => {
+        const scrollValue = data * 100;
+
+        const showImages = (arr) => {
+            setImages(prev => prev.map((item, index) =>
+                arr.indexOf(index) === -1
+                    ?
+                    { ...item, isActive: false }
+                    :
+                    { ...item, isActive: true }
+            ))
+        }
+
+        if (scrollValue < 0.4) {
+            showImages([]);
+        }
+        else if (scrollValue < 0.8) {
+            showImages([0]);
+        }
+        else if (scrollValue < 1.2) {
+            showImages([0, 1]);
+        }
+        else if (scrollValue < 1.6) {
+            showImages([0, 1, 2]);
+        }
+        else if (scrollValue < 2) {
+            showImages([0, 1, 2, 3]);
+        }
+        else if (scrollValue < 2.4) {
+            showImages([0, 1, 2, 3, 4]);
+        }
+        else if (scrollValue < 2.8) {
+            showImages([0, 1, 2, 3, 4, 5]);
+        }
+    });
 
     return (
         <div className="w-full h-[82vh] text-center">
